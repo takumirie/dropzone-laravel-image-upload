05b2c29b567d3890f55dc3b53622ebbfe78e2554
Ivan Radunovic <ivan@codingo.me>
2015-08-14 00:48:53 +0200

    Initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+Route::get('/', ['as' => 'upload', 'uses' => 'ImageController@getUpload']);
+Route::post('upload', ['as' => 'upload-post', 'uses' =>'ImageController@postUpload']);
+Route::post('upload/delete', ['as' => 'upload-remove', 'uses' =>'ImageController@deleteUpload']);
---
+++ config/images.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'full_size'   => '/var/www/html/tuts/dropzone/public/images/full_size/',
+    'icon_size'   => '/var/www/html/tuts/dropzone/public/images/icon_size/',
+];
